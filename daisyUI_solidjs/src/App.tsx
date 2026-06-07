3e82c12e0b79bc0c4c14a36f099e71c8a226b248
devg1120 <devg1120@gmail.com>
2026-06-07 10:42:23 +0900

    up

---
+++ daisyUI_solidjs/src/App.tsx
@@ -0,0 +1,14 @@
+import { createSignal } from 'solid-js'
+import './App.css'
+
+function App() {
+return (
+  <>
+  <button class="btn">Button</button>
+  <button class="btn btn-primary">Button</button>
+  </>
+)
+}
+
+export default App
+
