365037150709af65e3dda6bf6872142328429604
devg1120 <devg1120@gmail.com>
2026-06-24 02:37:36 +0000

    up

--- solidui-pro/src/components/framework/dividing.tsx
+++
@@ -1,5 +0,0 @@
-const Dividing = () => {
-	return <div class="w-full border-t border-muted " />
-}
-
-export default Dividing
--- solidui-pro/src/components/framework/table/index.tsx
+++
@@ -1,2 +0,0 @@
-export { default as ProTable } from './pro-table'
-export type { ProTableProps } from './types' 
