e3e50bebab6f0e116c21780d3cb6618292f80c5a
西草昌市 <devg1120@gmail.com>
2026-06-25 21:17:12 +0900

    up

--- kobalte-solid-dashboard2/src/components/kobalte/NumberField/NumberField.tsx
+++ kobalte-solid-dashboard2/src/components/kobalte/NumberField/NumberField.tsx
@@ -1,6 +1,11 @@
 import { NumberField } from "@kobalte/core/number-field";
-import { ArrowUpIcon } from 'lucide-solid';
-import { ArrowDownIcon } from 'lucide-solid';
+//import { ArrowUpIcon } from 'lucide-solid';
+//import { ArrowDownIcon } from 'lucide-solid';
+
+//https://solid-icons.vercel.app/search/package/bi/arrow
+
+import { BiSolidDownArrow as Down } from 'solid-icons/bi'
+import { BiSolidUpArrow as Up} from 'solid-icons/bi'
 
 import "./style.css";
 export function NumberField_() {
@@ -11,8 +16,8 @@ export function NumberField_() {
       </NumberField.Label>
       <div class="number-field__group">
         <NumberField.Input class="number-field__input" />
-        <NumberField.IncrementTrigger aria-label="Increment" class="number-field__increment"><ArrowUpIcon/></NumberField.IncrementTrigger>
-        <NumberField.DecrementTrigger aria-label="Decrement" class="number-field__decrement"><ArrowDownIcon/></NumberField.DecrementTrigger>
+        <NumberField.IncrementTrigger aria-label="Increment" class="number-field__increment"><Up/></NumberField.IncrementTrigger>
+        <NumberField.DecrementTrigger aria-label="Decrement" class="number-field__decrement"><Down/></NumberField.DecrementTrigger>
       </div>
     </NumberField>
   );
